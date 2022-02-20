cca6b6df1ebe53b660b7f271a22d077a57cd53da
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2022-02-19 20:35:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from bot import DemBot
 
 bot = DemBot(token=os.environ['TOKEN'],
              intents=discord.Intents.all(),
-             activity=discord.Game("demung"),
+             activity=discord.Activity(
+                 type=discord.ActivityType.watching, name='demung'),
              help_command=None)
 
 bot.starter()
